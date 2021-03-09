1c4ac0033925b130fa65ea9d3c9d9e2853ee9ba7
swizz <swizze@bk.ru>
2021-03-09 18:58:55 +0300

    ParserSqlRuController заменил @Controller -> @RestController проблема с ошибкой ушла

--- src/main/java/ru/usovsu/parser/controllers/ParserSqlRuController.java
+++ src/main/java/ru/usovsu/parser/controllers/ParserSqlRuController.java
@@ -2,10 +2,7 @@ package ru.usovsu.parser.controllers;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.usovsu.parser.entity.TopicSqlRu;
 import ru.usovsu.parser.service.ParserSqlRuService;
 
